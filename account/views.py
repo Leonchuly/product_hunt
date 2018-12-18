a4797d92b55c8584d1e72e45844bcaca3c99bc19
Leonchuly <5628113@qq.com>
2018-12-18 11:53:47 +0800

    解决了DoesNotExist报错的问题

--- account/views.py
+++ account/views.py
@@ -4,17 +4,17 @@ from django.contrib.auth.models import User
 
 def signup(request):
     if request.method == 'GET':
-        return render(request,'signup.html')
+        return render(request, 'signup.html')
     elif request.method == 'POST':
         user_name = request.POST['用户名']
         password1 = request.POST['密码']
         password2 = request.POST['确认密码']
         try:
-            User.objecrs.get(username=user_name)
-            return render(request, 'signup.html', {'用户名错误':'该用户名已存在'})
-        except User.USERNAME_FIELD:
+            User.objects.get(username=user_name)
+            return render(request, 'signup.html', {'用户名错误': '该用户名已存在'})
+        except User.DoesNotExist:
             if password1 == password2:
-                User.objects.create(username=user_name, password=password1)
+                User.objects.create_user(username=user_name, password=password1)
                 return redirect('main')
             else:
-                return render(request, 'signup.html', {'密码错误':'两次输入的密码不一致'})
+                return render(request, 'signup.html', {'密码错误': '两次输入的密码不一致!'})
